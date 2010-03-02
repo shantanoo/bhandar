afc9c54583aa3c52c511e389315d5098c0db4d24
Shantanoo <shantanoo@dhoomketu.local>
2010-03-02 13:58:13 +0530

    sample factory code

---
+++ Factory.py
@@ -0,0 +1,26 @@
+
+class Factory:
+    def register(self, methodName, constructor, *args, **kargs):
+        """register a constructor"""
+        _args = [constructor]
+        _args.extend(args)
+        setattr(self, methodName,apply(Functor,_args, kargs))
+
+    def unregister(self, methodName):
+        """unregister a constructor"""
+        delattr(self, methodName)
+
+class Functor:
+    def __init__(self, function, *args, **kargs):
+        assert callable(function), "function should be a callable obj"
+        self._function = function
+        self._args = args
+        self._kargs = kargs
+
+    def __call__(self, *args, **kargs):
+        """call function"""
+        _args = list(self._args)
+        _args.extend(args)
+        _kargs = self._kargs.copy()
+        _kargs.update(kargs)
+        return apply(self._function,_args,_kargs)
