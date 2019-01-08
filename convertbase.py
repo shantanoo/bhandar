7f8f585d2def9747b9d420f33124387362cf6d29
शंतनू <shantanoo@gmail.com>
2019-01-08 13:45:26 +0530

    Improved logic

--- convertbase.py
+++ convertbase.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 import sys
+import string
 
 def convert(no, ibase, obase):
-    newno = int(no,ibase)
+    newno = int(no, ibase)
     if obase == 10:
         return str(newno)
-    dict = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'G', 17: 'H', 18: 'I', 19: 'J', 20: 'K', 21: 'L', 22: 'M', 23: 'N', 24: 'O', 25: 'P', 26: 'Q', 27: 'R', 28: 'S', 29: 'T', 30: 'U', 31: 'V', 32: 'W', 33: 'X', 34: 'Y', 35: 'Z'}
+    data = dict(enumerate(string.digits+string.ascii_uppercase))
     ret = []
     while newno:
-        ret.insert(0,str(dict[newno%obase]))
+        ret.insert(0, str(data[newno % obase]))
         newno = int(newno / obase)
     return ''.join(ret)
 
 
 if len(sys.argv) != 4:
-    print "Invalid number of parameters passed"
-    print "Usage:"
-    print sys.argv[0]+" <number> <input_base> <output_base>"
+    print("Invalid number of parameters passed")
+    print("Usage:")
+    print(sys.argv[0] + " <number> <input_base> <output_base>")
     sys.exit(1)
 
-
-print convert(sys.argv[1],int(sys.argv[2]), int(sys.argv[3]))
+print(convert(sys.argv[1], int(sys.argv[2]), int(sys.argv[3])))
